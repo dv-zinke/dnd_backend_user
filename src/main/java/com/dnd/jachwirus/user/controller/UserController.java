e803fe02c982b9777cdba65dd367d9c04f00018b
hj <dv.zinke@gmail.com>
2020-09-01 02:01:27 +0900

    feat : 회원가입 이메일 있을시 에러 처리

--- src/main/java/com/dnd/jachwirus/user/controller/UserController.java
+++ src/main/java/com/dnd/jachwirus/user/controller/UserController.java
@@ -2,15 +2,16 @@ package com.dnd.jachwirus.user.controller;
 
 import com.dnd.jachwirus.user.config.JwtTokenProvider;
 import com.dnd.jachwirus.user.domain.User;
+import com.dnd.jachwirus.user.exception.RestException;
 import com.dnd.jachwirus.user.repository.UserRepository;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,12 +22,20 @@ public class UserController {
 
     // 회원가입
     @PostMapping("/join")
-    public Long join(@RequestBody Map<String, String> user) {
+    public User join(@RequestBody Map<String, String> user) throws RestException {
+
+        Optional<User> foundUser = userRepository.findByEmail(user.get("email"));
+
+        if(foundUser.isPresent()) {
+            throw new RestException(HttpStatus.BAD_REQUEST, "존재 하는 이메일입니다.");
+        }
+
+
         return userRepository.save(User.builder()
                 .email(user.get("email"))
                 .password(passwordEncoder.encode(user.get("password")))
                 .roles(Collections.singletonList("ROLE_USER")) // 최초 가입시 USER 로 설정
-                .build()).getId();
+                .build());
     }
 
     // 로그인
